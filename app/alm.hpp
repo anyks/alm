67d4bf84d0e6623340735d59802b136335043995
forman <forman@anyks.com>
2020-08-04 18:06:32 +0300

    Исправление бага обучения языковой модели при выборе промежуточных форматов данных

--- app/alm.hpp
+++ app/alm.hpp
@@ -9,7 +9,7 @@
 // Название языковой модели
 #define ANYKS_LM_NAME "alm"
 // Версия приложения
-#define ANYKS_LM_VERSION "3.2.2"
+#define ANYKS_LM_VERSION "3.2.3"
 // Автор приложения
 #define ANYKS_LM_AUTHOR "Yuriy Lobarev"
 // Телефон автора
