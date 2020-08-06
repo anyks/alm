bbd35b95e065ca58e4ab5e5bdd70852a71656175
forman <forman@anyks.com>
2020-08-06 16:59:28 +0300

    Доработка дампера бинарного формата

--- app/alm.hpp
+++ app/alm.hpp
@@ -9,7 +9,7 @@
 // Название языковой модели
 #define ANYKS_LM_NAME "alm"
 // Версия приложения
-#define ANYKS_LM_VERSION "3.2.4"
+#define ANYKS_LM_VERSION "3.2.5"
 // Автор приложения
 #define ANYKS_LM_AUTHOR "Yuriy Lobarev"
 // Телефон автора
