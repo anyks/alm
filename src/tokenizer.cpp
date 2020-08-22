036dc7d5a4de24e80e664bca84358fd623df3230
forman <forman@anyks.com>
2020-08-22 19:34:56 +0300

    Демо-версия 3.2.7

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -44,7 +44,7 @@ void anyks::Tokenizer::restore(const wstring & first, const wstring & second, ws
 				// Если длина слова больше 1-го символа, значит это слово
 				if(word.length() > 1){
 					// Определяем, число это или нет
-					result = (this->alphabet->isNumber(word) || (this->alphabet->isDecimal(word) ? type_t::num : type_t::word);
+					result = (this->alphabet->isNumber(word) || this->alphabet->isDecimal(word) ? type_t::num : type_t::word);
 				// Если это всего один символ
 				} else {
 					// Получаем символ токена в нижнем регистре
